6a59ebf64a45bc7177a1d7f6911dcf93aaf43199
Садова Валерия Андреевна <SadovaVA@vseinstrumenti.ru>
2022-08-08 08:57:42 +0300

    решила первую задачу

--- Zadacha1_max_min/Program.cs
+++ Zadacha1_max_min/Program.cs
@@ -3,3 +3,21 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+Console.Clear(); //очистка терминала при запуске
+Console.WriteLine("Привет, друг! Определим какое из двух числе больше, а какое меньше? :)");
+Console.WriteLine("Начинаем? Приготовься придумать два целых числа и ввести их");
+Console.WriteLine("Введите ПЕРВОЕ целое число: ");
+int num1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите ВТОРОЕ целое число: ");
+int num2=Convert.ToInt32(Console.ReadLine());
+
+if (num1>num2)
+    {
+       Console.WriteLine("{0} наибольшее число", num1);
+    }
+else if (num2>num1)
+ 
+    {
+        Console.WriteLine("{0} наибольшее число ", num2);
+    }
+else {Console.WriteLine("Введенные числа равны");}
